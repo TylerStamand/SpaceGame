043dfe33279e03bce47b365a8bff2a441a29c33d
Tyler St. Amand <stamandtyler@gmail.com>
2020-06-14 12:31:16 -0400

    initial

---
+++ Scripts/Game1.cs
@@ -0,0 +1,93 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace myGame
+{
+
+    public enum Direction {
+        forward,
+        backward,
+        left,
+        right
+    }
+
+    public class Game1 : Game
+    {
+        public static GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+  
+        private Ship ship;
+
+
+        private KeyboardState state;
+
+        
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ship = new Ship(Content.Load<Texture2D>("ship"), new Vector2(240,240), 3, MathHelper.ToRadians(180), .1f);
+            
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.A)) {
+                ship.Move(Direction.right);
+            }
+            else if (state.IsKeyDown(Keys.D))
+            {
+                ship.Move(Direction.left);
+            }
+            if(state.IsKeyDown(Keys.W)) {
+                ship.Move(Direction.forward);
+            }
+            else if (state.IsKeyDown(Keys.S)) {
+                ship.Move(Direction.backward);
+            }
+            if(state.IsKeyDown(Keys.Space)) {
+                ship.Shoot();
+            }
+
+            base.Update(gameTime);
+            
+       
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.Gray);
+            
+      
+            spriteBatch.Begin();
+            ship.Draw(spriteBatch);
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Scripts/Ship.cs
@@ -0,0 +1,102 @@
+using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace myGame {
+    public class Ship {
+        public Texture2D Texture {get; set;}
+        public Vector2 Position {get; set;}
+        public float Rotation {get; set;}
+        public float Speed {get; set;}
+        public float RotationSpeed {get; set;}
+
+        private int width;
+        private int height;
+
+        public Ship(Texture2D texture, Vector2 position, float speed = 1, float rotation = 0, float rotationSpeed = 1) {
+            Texture = texture;
+            Position = position;
+            Rotation = rotation;
+            Speed = speed;
+            RotationSpeed = rotationSpeed;
+
+            width = Texture.Width;
+            height = Texture.Height;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            Rectangle sourceRectangle = new Rectangle(0,0, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)Position.X, (int)Position.Y, width, height);
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White, Rotation, new Vector2(width/2, height/2), SpriteEffects.None, 0f);
+        }
+
+
+        public void Move(Direction direction) {
+
+
+            if(direction == Direction.left)  {
+                this.Rotation = (this.Rotation + RotationSpeed) % (2 * MathHelper.Pi);
+            }
+            else if (direction == Direction.right)
+            {
+                this.Rotation = (this.Rotation + (-1 * RotationSpeed)) % (2 * MathHelper.Pi);
+            }
+            float changeY = 0;
+            float changeX = 0;
+
+            if(direction == Direction.forward) {
+                changeY += MathF.Cos(Rotation) * Speed;
+                changeX -= MathF.Sin(Rotation) * Speed;
+            }
+            else if (direction == Direction.backward)
+            {
+                changeY -= MathF.Cos(Rotation) * Speed;
+                changeX += MathF.Sin(Rotation) * Speed;
+            }
+
+           
+            //Check Bounds and Set 
+            int screenW = Game1.graphics.PreferredBackBufferWidth ;
+            int screenH = Game1.graphics.PreferredBackBufferHeight;
+
+            float X, Y;
+
+            if (changeX + Position.X > screenW) {
+                X = screenW;
+            }
+            else if (changeX + Position.X < 0) {
+                X = 0;
+            }
+            else {
+                X = changeX + Position.X;
+            }
+
+            if (changeY + Position.Y > screenH) {
+                Y = screenH;
+            }
+            else if (changeY + Position.Y < 0) {
+                Y = 0;
+            }
+            else {
+                Y = changeY + Position.Y;
+            }
+
+            Position = new Vector2(X, Y);
+        }
+
+        public void Shoot () {
+            
+        }
+    }
+}
+        
+
+            
+
+
+         
+           
+        
+        
